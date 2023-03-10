3574e6fd2c097ef2a70626a3050b6c59e1759d00
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-03-10 15:31:09 +0100

    set up router and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 import './App.css';
+import HomePage from "./home/HomePage";
+import PortfolioPage from "./portfolio/PortfolioPage";
+import NotFoundPage from "./shared/NotFoundPage";
+import CVPage from "./cv/CVPage";
+import ContactPage from "./contact/ContactPage";
+import AboutPage from "./about/AboutPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <NavLink to="/">
+          <span>Neurothrone</span>
+        </NavLink>
+        <NavLink to="/about">
+          <span>About</span>
+        </NavLink>
+        <NavLink to="/Contact">
+          <span>Contact</span>
+        </NavLink>
+        <NavLink to="/CV">
+          <span>CV</span>
+        </NavLink>
+        <NavLink to="/portfolio">
+          <span>Portfolio</span>
+        </NavLink>
       </header>
-    </div>
+
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/contact" element={<ContactPage/>}/>
+        <Route path="/cv" element={<CVPage/>}/>
+        <Route path="/portfolio" element={<PortfolioPage/>}/>
+        <Route path="/*" element={<NotFoundPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
