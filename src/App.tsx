4586bd1f194a7a00edfc5fefeda75334389ddc2d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-03-11 18:39:31 +0100

    set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import {
-  BrowserRouter as Router,
-  NavLink,
+  BrowserRouter,
   Route,
   Routes,
 } from "react-router-dom";
@@ -10,41 +9,41 @@ import {
 import './App.css';
 import HomePage from "./home/HomePage";
 import PortfolioPage from "./portfolio/PortfolioPage";
-import NotFoundPage from "./shared/NotFoundPage";
+import NotFoundPage from "./components/NotFoundPage";
 import CVPage from "./cv/CVPage";
 import ContactPage from "./contact/ContactPage";
 import AboutPage from "./about/AboutPage";
+import Header from "./components/Header";
+import ProjectView from "./portfolio/ProjectView";
+import CategoryView from "./portfolio/CategoryView";
+import AdminPage from "./admin/AdminPage";
 
 function App() {
   return (
-    <Router>
-      <header>
-        <NavLink to="/">
-          <span>Neurothrone</span>
-        </NavLink>
-        <NavLink to="/about">
-          <span>About</span>
-        </NavLink>
-        <NavLink to="/Contact">
-          <span>Contact</span>
-        </NavLink>
-        <NavLink to="/CV">
-          <span>CV</span>
-        </NavLink>
-        <NavLink to="/portfolio">
-          <span>Portfolio</span>
-        </NavLink>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header/>
 
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/about" element={<AboutPage/>}/>
-        <Route path="/contact" element={<ContactPage/>}/>
-        <Route path="/cv" element={<CVPage/>}/>
-        <Route path="/portfolio" element={<PortfolioPage/>}/>
-        <Route path="/*" element={<NotFoundPage/>}/>
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+          <Route path="/contact" element={<ContactPage/>}/>
+          <Route path="/cv" element={<CVPage/>}/>
+          <Route path="/portfolio" element={<PortfolioPage/>}>
+            <Route path=":categoryId" element={<CategoryView/>}>
+              <Route path=":projectId" element={<ProjectView/>}/>
+            </Route>
+            <Route index element={<h3>Select a category</h3>}/>
+          </Route>
+          <Route path="/admin" element={<AdminPage/>}/>
+          <Route path="/*" element={<NotFoundPage/>}/>
+        </Routes>
+      </BrowserRouter>
+
+      <footer className="container">
+        &copy;{new Date().getFullYear()} | <a href="https://neurothrone.tech/">Neurothrone</a>
+      </footer>
+    </div>
   );
 }
 
